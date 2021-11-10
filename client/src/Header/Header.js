798850f44f701d3071b6b7eb229dd050eae1f40e
Daniel Gorbatov <dg20@uw.edu>
2021-11-09 20:12:10 -0800

    Got header partialy done

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -6,10 +6,13 @@ function Header() {
     <div className="header">
       <h1>UNIStats</h1>
 
-      <form>
-        <Icon icon="akar-icons:search" width="0.8vw" color="#575757"/>
-        <input type="text" placeholder="Search University"/>
-      </form>
+      <section>
+        <p>All Stats</p>
+
+        <p>Add Stat</p>
+
+        <p>Account</p>
+      </section>
     </div>
   )
 }
